092b6d4b59d61e2470ae092ab5a10f2821d5cc18
rekar <rekartarq49@gmail.com>
2023-02-17 22:03:11 +0300

    this is navigate to index page

--- src/layouts/Guest.jsx
+++ src/layouts/Guest.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Navigate, Outlet } from 'react-router-dom'
+import { useContextprovider } from '../Context/ContextProvider'
 
 const Guest = () => {
+  const { token } = useContextprovider();
+  if (token) {
+    return <Navigate to="/"/>
+  }
   return (
     <div>
       
